3786888d741c159ed3918e8eae7663ec632b6257
juan-vazquez-20 <juan.vazquez0290@alumnos.udg.mx>
2025-06-02 18:19:54 -0600

    darkMode

--- bienesraices_inicio/src/js/app.js
+++ bienesraices_inicio/src/js/app.js
@@ -1,8 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
-
     eventListener();
+    darkmode();
 });
+
+function darkmode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }
+    else {
+        document.body.classList.remove('dark-mode');
+    }
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }
+        else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+     const botonDarkMode = document.querySelector('.dark-mode-boton');
+     botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+     });
+};
 
 function eventListener(){
     const mobileMenu = document.querySelector('.mobile-menu');
@@ -14,3 +36,4 @@ function navegacionResponsive() {
 
     navagacion.classList.toggle('mostrar');
 }
+
